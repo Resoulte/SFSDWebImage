d97efeeb3566b66ce9e0ee71b4b8e2c44f4d82f7
Eric <sferic@163.com>
2016-08-04 19:50:28 +0800

    封装了两个常用的功能

--- SFSDWebImage/SFSDWebImage/ViewController.m
+++ SFSDWebImage/SFSDWebImage/ViewController.m
@@ -7,18 +7,53 @@
 //
 
 #import "ViewController.h"
+#import "UIImageView+SFSDWebImage.h"
 
 @interface ViewController ()
+/**
+ *  拿到storyboard里面的UIImageView
+ */
+@property (weak, nonatomic) IBOutlet UIImageView *iconImageV;
 
 @end
-
+static NSString *urlStr = @"http://img5.imgtn.bdimg.com/it/u=3063552411,3030228420&fm=206&gp=0.jpg";
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    // 异步加载图片
+//    [self downloadImage];
+    
+    // 异步加载图片，监听下载进度是否成功与失败
+    [self downloadImageWithProgress];
+    
+    
 }
 
+/**
+ *  异步加载图片
+ */
+- (void)downloadImage {
+    
+    [self.iconImageV downloadImage:urlStr placeholder:@"placeholder"];
+    
+}
+
+/**
+ *  异步加载图片，监听下载进度是否成功与失败
+ */
+- (void)downloadImageWithProgress {
+
+    [self.iconImageV downloadImage:urlStr placeholder:@"placeholder" success:^(UIImage *image) {
+        NSLog(@"%@", image);
+    } failed:^(NSError *error) {
+        NSLog(@"%@", error);
+    } progress:^(CGFloat progress) {
+        NSLog(@"%@", @(progress));
+    }];
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
